4ec1e154f15006d23c96648187f568ec231e0c4b
Joaquin Rosendo <joaquinrosendo@live.com>
2023-08-08 16:36:55 -0500

    display dog info

--- Dog.js
+++ Dog.js
@@ -1,1 +1,21 @@
 // Create the Dog class here
+
+class Dog {
+    constructor(dogData){
+        Object.assign(this, dogData);
+    }
+
+    getDogHtml(){
+        const {name, avatar, age, bio} = this;
+
+        return `<div class="dog-card">
+                    <img src="${avatar}">
+                    <div class="dog-info">
+                        <h2 class="dog-name">${name}, ${age}</h2>
+                        <p class="dog-bio">${bio}</p>
+                    </div>
+                </div>`
+    }
+}
+
+export default Dog;
